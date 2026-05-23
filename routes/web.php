2cdda383cf42d687884c244e568d6fa3743ed7b9
TOP <salsabeel1053.003s@gmail.com>
2026-05-23 16:48:00 +0300

    تعديل وإضافة ميزات جديدة في مشروع قائمة المهام

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+// --- مسارات صفحة About ---
 
 // عند فتح الصفحة لأول مرة (GET)
 Route::get('/about', function () {
@@ -15,14 +18,9 @@ Route::get('/about', function () {
     return view('about', compact('name', 'departments'));
 });
 
-// عند إرسال الفورم (POST)
+// عند إرسال فورم صفحة About (POST)
 Route::post('/about', function (Request $request) {
-    // إذا كنت تريد أن يظل الاسم "أحمد" حتى بعد الضغط على Send:
     $name = 'Ahmad';
-
-    // أما إذا كنت تريد استلام الاسم من الذي يكتبه المستخدم في الحقل:
-    // $name = $request->input('name');
-
     $departments = [
         '1' => 'Technical',
         '2' => 'Financial',
@@ -30,4 +28,48 @@ Route::post('/about', function (Request $request) {
     ];
 
     return view('about', compact('name', 'departments'));
+});
+
+
+// --- مسارات صفحة Tasks و Create ---
+
+Route::get('tasks', function(){
+    return view('tasks');
+});
+
+
+
+// 2. مسار لاستقبال البيانات الفعلي من الفورم (POST) عند الضغط على زر الإرسال
+Route::post('create', function() {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+
+    return redirect()->back();
+});
+
+Route::get('tasks', function() {
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks', compact('tasks'));
+});
+
+Route::post('delete/{id}', function($id) {
+    DB::table('tasks')->where('id', $id)->delete();
+
+    return redirect()->back();
+});
+
+Route::post('edit/{id}', function($id) {
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+
+    return view('tasks', compact('task', 'tasks'));
+});
+
+Route::post(uri: 'update', action: function() {
+    $id = $_POST['id'];
+
+    DB::table(table: 'tasks')->where('id', '=', $id)->update(['name' => $_POST['name'] ]);
+    return redirect('tasks');
+
 });
